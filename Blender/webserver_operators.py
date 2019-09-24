a59c43d02bdcc88bb04b97f353472bf9bcee66ab
Dan Amador <danamadorpe@gmail.com>
2019-09-24 22:22:12 +0200

    Create webserver_operators.py

---
+++ Blender/webserver_operators.py
@@ -0,0 +1,26 @@
+from bpy.types import Operator
+
+
+class Start(Operator):
+    """Start WebSocket server"""
+    bl_idname = "websocket_server.start"
+    bl_label = "Start WebSocket server"
+    
+    def execute(self, context):
+        #addon_prefs = context.user_preferences.addons[__name__].preferences
+        #if not start_server(str(addon_prefs.host), int(addon_prefs.port)):
+        #    self.report({"ERROR"}, "The server is already started.")
+        #    return {"CANCELLED"}
+        return {"FINISHED"}
+
+class Stop(Operator):
+    """Stop WebSocket server"""
+    bl_idname = "websocket_server.stop"
+    bl_label = "Stop WebSocket server"
+    
+    def execute(self, context):
+        #if not stop_server():
+        #   self.report({"ERROR"}, "The server is not started.")
+        #  return {"CANCELLED"}
+        return {"FINISHED"}
+    
