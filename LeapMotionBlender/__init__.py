1cae7e5517f822e457acfbeaed6ff544fad8b8dd
Dan Amador <danamadorpe@gmail.com>
2019-09-29 11:43:13 +0200

    Added Nuget Package manager and installed websocket sharp and newtonsoft json

--- LeapMotionBlender/__init__.py
+++ LeapMotionBlender/__init__.py
@@ -1,4 +1,3 @@
-# context.area: VIEW_3D
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
--- Unity/Assets/Communicator/Communicator.cs
+++ Unity/Assets/Communicator/Communicator.cs
@@ -1,9 +1,8 @@
 ﻿using Leap.Unity;
-using SocketIO;
 using UnityEngine;
 
 namespace Communicator {
-    [RequireComponent(typeof(SocketIOComponent))]
+    [RequireComponent(typeof(SocketComponent))]
     public class Communicator : MonoBehaviour {
         public RigidHand rightHand, leftHand;
         private SocketPacket _sp;
@@ -13,22 +12,21 @@ namespace Communicator {
 
         private float _lastSample;
 
-        private SocketIOComponent _socketIoComponent;
+        private SocketComponent _socketComponent;
 
         void Start() {
             _sp = new SocketPacket();
             _lastSample = Time.time;
             ChangeSampleRate(sampleRate);
-            _socketIoComponent = GetComponent<SocketIOComponent>();
-            _socketIoComponent.On("boop",
-                (SocketIOEvent e) => { Debug.Log("Fuck me "); });
+            _socketComponent = GetComponent<SocketComponent>();
         }
 
         void LateUpdate() {
             if (Time.time - _lastSample > _timePerSample) {
                 _sp.UpdateHands(leftHand, rightHand);
-                if (_socketIoComponent.isActiveAndEnabled && _socketIoComponent.IsConnected) {
-                    _socketIoComponent.Emit("unityBones", _sp.ToJson());
+            
+                if(_socketComponent.isActiveAndEnabled){
+                    _socketComponent.Send(_sp.ToJson());
                 }
 
                 _lastSample = Time.time;
