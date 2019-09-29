7f375155ba81c2453727ad260aa6af47b9adb575
Dan Amador <danamadorpe@gmail.com>
2019-09-29 11:44:41 +0200

    Unity to blender communication finally works

---
+++ Unity/Assets/Communicator/SocketComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using WebSocketSharp;
+
+namespace Communicator {
+    public class SocketComponent : MonoBehaviour {
+        private WebSocket _webSocket;
+
+        public string host = "localhost";
+        public int port = 4567;
+
+        void Start() {
+            _webSocket = new WebSocket($"ws://{host}:{port}");
+            _webSocket.Connect();
+            _webSocket.OnMessage += (sender, e) => InterpretMessage(e.Data);
+        }
+
+        private void InterpretMessage(string data) {
+            Debug.Log(data);
+        }
+
+        public void Send(string data) {
+            _webSocket.Send(data);
+        }
+
+        void Update() { }
+
+    }
+}
