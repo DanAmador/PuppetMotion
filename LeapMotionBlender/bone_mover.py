490840b2250313d5325ae0b25b89cfa1dcd0088b
Dan Amador <danamadorpe@gmail.com>
2019-10-06 23:30:12 +0200

    Added readme with results

--- LeapMotionBlender/bone_mover.py
+++ LeapMotionBlender/bone_mover.py
@@ -33,11 +33,13 @@ def move_bones():
 
                 
                 if props.recording:
-                    if props.frame_counter % props.record_rate == 0:
+                    if props.frame_counter % props.record_rate == 0 and props.frame_counter != 0:
                         if leap.rot_pos[1]:
                             bone.keyframe_insert(data_path="location",frame=props.frame_counter + props.start_frame)
                         if leap.rot_pos[0]:
-                            bone.keyframe_insert(data_path="rotation",frame=props.frame_counter + props.start_frame)
+                            for idx, axis in enumerate(axes):
+                                if leap.rot_select[idx]:
+                                    bone.keyframe_insert(data_path="rotation_euler",frame=props.frame_counter + props.start_frame, index=idx)
             
             if props.recording:
                 props.frame_counter += 1
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Leap Motion Bone Poser
+### This repo consists of 2 projects acting as a client and server using websockets.
+
+### The purpose of this project is to be able to animate my rigs programatically. 
+
+# Requisites
+### To install the Blender Plugin you also need to install the [Websockets](https://websockets.readthedocs.io/en/stable/intro.html) library in your Blender 2.8+ Python environment. 
+
+
+# Demo
+![](./demo/one.gif)
+![](./demo/giphy.gif)
