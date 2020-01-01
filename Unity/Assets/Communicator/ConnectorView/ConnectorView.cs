b52dbc9d2ab4ac6417c76131408c25f3cd722e17
Dan Amador <danamadorpe@gmail.com>
2020-01-01 11:59:33 +0100

    small refactor

--- Unity/Assets/Communicator/ConnectorView/ConnectorView.cs
+++ Unity/Assets/Communicator/ConnectorView/ConnectorView.cs
@@ -1,62 +1,50 @@
 ﻿using Communicator;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ConnectorView : MonoBehaviour
-{
+public class ConnectorView : MonoBehaviour {
     private SocketComponent socketComponent;
 
     private InputField host, port;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         socketComponent = FindObjectOfType<SocketComponent>();
-        if ((object)socketComponent == null)
-        {
+        if (socketComponent == null) {
             Debug.LogError("Cannot find the socket component");
         }
-        else
-        {
+        else {
             InitializeChildren();
         }
     }
 
-    private void InitializeChildren()
-    {
+    private void InitializeChildren() {
         Transform aux;
 
-        aux = this.transform.Find("Host");
-        if ((object)aux == null)
-        {
+        aux = transform.Find("Host");
+        if (aux == null) {
             Debug.LogError("Cannot find the host text object");
         }
-        else
-        {
+        else {
             host = aux.GetComponent<InputField>();
             host.text = socketComponent.host;
         }
 
-        aux = this.transform.Find("Port");
-        if ((object)aux == null)
-        {
+        aux = transform.Find("Port");
+        if (aux == null) {
             Debug.LogError("Cannot find the port text object");
         }
-        else
-        {
+        else {
             port = aux.GetComponent<InputField>();
             port.text = socketComponent.port.ToString();
         }
     }
 
-    public void Connect()
-    {
-        socketComponent.NewConnection(
-            host.text,
-            int.Parse(port.text)
-            );
+    public void Connect() {
+        try {
+            socketComponent.SetUpConnection(host.text, int.Parse(port.text));
+        }
+        catch {
+            Debug.LogError($"Unable to find ws://{host.text}:{port.text}");
+        }
     }
 }
--- Unity/Assets/Communicator/SocketComponent.cs
+++ Unity/Assets/Communicator/SocketComponent.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using WebSocketSharp;
 
 namespace Communicator {
-    public partial class SocketComponent : MonoBehaviour {
+    public class SocketComponent : MonoBehaviour {
         private WebSocket _webSocket;
 
         public string host = "localhost";
@@ -15,12 +14,32 @@ namespace Communicator {
         }
 
         public void Send(string data) {
-            if ((object)_webSocket != null)
-            {
+            if (_webSocket != null) {
                 _webSocket.Send(data);
             }
         }
 
+        public void SetUpConnection(string host, int port) {
+            DisposeConnection();
 
+            this.host = host;
+            this.port = port;
+            _webSocket = new WebSocket($"ws://{host}:{port}");
+            _webSocket.Connect();
+
+            _webSocket.OnMessage += InterpretMessageCallback;
+        }
+
+        private void DisposeConnection() {
+            if (_webSocket != null) {
+                _webSocket.OnMessage -= InterpretMessageCallback;
+                _webSocket.Close();
+            }
+        }
+
+
+        private void InterpretMessageCallback(object sender, MessageEventArgs e) {
+            InterpretMessage(e.Data);
+        }
     }
 }
